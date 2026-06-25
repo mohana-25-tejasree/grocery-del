d48a6097adb501acd0990fd5b3f3f5ab9b98da7a
mohana-25-tejasree <mohana25cbb@gmail.com>
2026-06-25 13:43:15 +0530

    project completed

--- server/controllers/orderController.ts
+++ server/controllers/orderController.ts
@@ -1,7 +1,7 @@
 import { Request,Response } from "express"
 import { prisma } from "../config/prisma.js";
 import { inngest } from "../inngest/index.js";
-import Stripe from 'stripe'
+
 
 export const createOrder = async (req:Request,res:Response)=>{
     const{items,shippingAddress,paymentMethod}=req.body;
@@ -56,26 +56,6 @@ export const createOrder = async (req:Request,res:Response)=>{
     })
 
     if(paymentMethod === "card"){
-        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
-        const session = await stripe.checkout.sessions.create({
-            success_url: `${req.headers.origin}/orders?clearCart= true`,
-            cancel_url: `${req.headers.origin}/checkout`,
-            line_items: [
-                {
-                    price_data: {
-                        currency:"usd",
-                        product_data:{
-                            name:"Payment Groceries"
-                        },
-                        unit_amount: Math.round(total *100)
-                    },
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            metadata:{orderId:order.id}
-        });
-        return res.json({url: session.url})
 
     }
 
@@ -136,7 +116,7 @@ export const updateOrderStatus = async (req:Request,res:Response)=>{
     if (!order){
         return res.status(404).json({message:"Order not found"});
     }
-    res.json({order})
+    
 
     const history =(Array.isArray(order.statusHistory)?order.statusHistory:[]) as any[];
     history.push({status,note:note || `Order ${status.toLowerCase()}`,timestamp: new Date()})
